d3172be5fa8725348122d557b4aa50c6189fb985
陈松 <chensong@weidian.com>
2026-08-27 10:38:24 +0800

    add zabbix 95 cal

--- utils/scheduler.py
+++ utils/scheduler.py
@@ -18,7 +18,7 @@ logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     handlers=[
-        logging.FileHandler('../logs/scheduler.log'),
+        logging.FileHandler('logs/scheduler.log'),
         logging.StreamHandler()
     ]
 )
